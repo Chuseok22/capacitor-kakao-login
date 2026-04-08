24013f0563c979e4fdc4b39e43ef199728e7e522
Baek Jihoon <133375485+Chuseok22@users.noreply.github.com>
2026-04-08 12:47:27 +0900

    feat: delete KakaoLoginPlugin.swift

--- ios/Sources/KakaoLoginPlugin/KakaoLoginPlugin.swift
+++
@@ -1,3 +0,0 @@
-// 이 파일은 더 이상 사용되지 않습니다.
-// 소스는 ios/Sources/Chuseok22CapacitorKakaoLogin/KakaoLoginPlugin.swift 로 이동되었습니다.
-// This file is intentionally left empty to prevent duplicate symbol compilation errors.
